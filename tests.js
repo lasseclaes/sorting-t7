002a5fe430c4a688581bd809cdaca555b1cc07fd
Jonas Holbech <jofh@kea.dk>
2020-02-23 22:18:34 +0100

    played a bit with the metaphore

--- tests.js
+++ tests.js
@@ -1,11 +1,66 @@
-const template = document.querySelector("template").content;
-deck.forEach(card => {
-  const copy = template.cloneNode(true);
-  copy.querySelector("h1").innerHTML = card;
-  copy.querySelector(".flip-card").dataset.cardvalue = card.split("&")[0];
-  document.querySelector(".ex1").appendChild(copy);
-})
-const test = function () {
+class FLIP {
+  constructor(el) {
+    this.el = el;
+    this.first = null;
+    this.last = null;
+  }
+  logInitialPosition() {
+    this.first = this.el.getBoundingClientRect();
+  }
+  logFinalPosition() {
+    this.last = this.el.getBoundingClientRect();
+  }
+  animate(duration, delay) {
+    const deltaX = this.first.left - this.last.left;
+    const deltaY = this.first.top - this.last.top;
+    this.el.animate(
+      [
+        {
+          transformOrigin: "top left",
+          transform: `translate(${deltaX}px, ${deltaY}px)`
+        },
+        {
+          transformOrigin: "top left",
+          transform: "none"
+        }
+      ], {
+        duration: duration,
+        delay: delay,
+        easing: "ease-in-out",
+        fill: "both"
+      }
+    );
+  }
+}
+window.addEventListener("load", layOutDeck);
+
+function layOutDeck() {
+  const template = document.querySelector("template").content;
+  deck.forEach((card, index) => {
+    const copy = template.cloneNode(true);
+    copy.querySelector("h1").innerHTML = card;
+    copy.querySelector(".flip-card").dataset.cardvalue = card.split("&")[0];
+    copy.querySelector(".flip-card").style.zIndex = 13 * 4 - index;
+    document.querySelector(".deckStart").appendChild(copy);
+  });
+  dealCards();
+}
+
+function dealCards() {
+  document.querySelectorAll(".deckStart>*").forEach((card, index) => {
+
+    const x = new FLIP(card);
+    x.logInitialPosition();
+    document.querySelector(".ex1").appendChild(card);
+    x.logFinalPosition();
+    x.animate(300, index * 100);
+  })
+  document.querySelector("button").addEventListener("click", test)
+}
+
+
+
+function test() {
   let counter = 0;
   const intID = setInterval(() => {
     counter++;
