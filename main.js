7c1737b528fa7ef865a2750291ea454c012c4b6a
Lasse Claes Jacobsen <lacj@windows-885s76e.kea.local>
2020-02-24 10:24:11 +0100

    not sure here - merge conflict...

--- main.js
+++ main.js
@@ -1,65 +1,225 @@
-const deck = [
-  "6",
-  "5",
-  "8",
-  "6",
-  "6",
-  "4",
-  "11",
-  "6",
-  "12",
-  "12",
-  "3",
-  "11",
-  "3",
-  "10",
-  "5",
-  "12",
-  "1",
-  "2",
-  "7",
-  "11",
-  "10",
-  "7",
-  "8",
-  "4",
-  "2",
-  "1",
-  "3",
-  "8",
-  "12",
-  "10",
-  "3",
-  "7",
-  "7",
-  "5",
-  "4",
-  "2",
-  "4",
-  "5",
-  "11",
-  "9",
-  "9",
-  "10",
-  "9",
-  "9",
-  "2",
-  "8",
-  "1",
-  "1"
+const deck = [{
+    value: 11,
+    suit: "♤"
+  },
+  {
+    value: 10,
+    suit: "♡"
+  },
+  {
+    value: 8,
+    suit: "♤"
+  },
+  {
+    value: 6,
+    suit: "♧"
+  },
+  {
+    value: 4,
+    suit: "♡"
+  },
+  {
+    value: 9,
+    suit: "♧"
+  },
+  {
+    value: 4,
+    suit: "♢"
+  },
+  {
+    value: 6,
+    suit: "♤"
+  },
+  {
+    value: 13,
+    suit: "♤"
+  },
+  {
+    value: 13,
+    suit: "♢"
+  },
+  {
+    value: 6,
+    suit: "♡"
+  },
+  {
+    value: 4,
+    suit: "♤"
+  },
+  {
+    value: 11,
+    suit: "♧"
+  },
+  {
+    value: 7,
+    suit: "♡"
+  },
+  {
+    value: 8,
+    suit: "♡"
+  },
+  {
+    value: 5,
+    suit: "♡"
+  },
+  {
+    value: 8,
+    suit: "♧"
+  },
+  {
+    value: 2,
+    suit: "♧"
+  },
+  {
+    value: 12,
+    suit: "♡"
+  },
+  {
+    value: 9,
+    suit: "♢"
+  },
+  {
+    value: 13,
+    suit: "♡"
+  },
+  {
+    value: 3,
+    suit: "♧"
+  },
+  {
+    value: 5,
+    suit: "♢"
+  },
+  {
+    value: 12,
+    suit: "♤"
+  },
+  {
+    value: 1,
+    suit: "♢"
+  },
+  {
+    value: 2,
+    suit: "♡"
+  },
+  {
+    value: 3,
+    suit: "♢"
+  },
+  {
+    value: 2,
+    suit: "♤"
+  },
+  {
+    value: 9,
+    suit: "♤"
+  },
+  {
+    value: 5,
+    suit: "♤"
+  },
+  {
+    value: 7,
+    suit: "♤"
+  },
+  {
+    value: 11,
+    suit: "♡"
+  },
+  {
+    value: 12,
+    suit: "♢"
+  },
+  {
+    value: 7,
+    suit: "♢"
+  },
+  {
+    value: 6,
+    suit: "♢"
+  },
+  {
+    value: 10,
+    suit: "♧"
+  },
+  {
+    value: 1,
+    suit: "♤"
+  },
+  {
+    value: 12,
+    suit: "♧"
+  },
+  {
+    value: 10,
+    suit: "♤"
+  },
+  {
+    value: 13,
+    suit: "♧"
+  },
+  {
+    value: 1,
+    suit: "♧"
+  },
+  {
+    value: 7,
+    suit: "♧"
+  },
+  {
+    value: 3,
+    suit: "♤"
+  },
+  {
+    value: 5,
+    suit: "♧"
+  },
+  {
+    value: 9,
+    suit: "♡"
+  },
+  {
+    value: 8,
+    suit: "♢"
+  },
+  {
+    value: 11,
+    suit: "♢"
+  },
+  {
+    value: 4,
+    suit: "♧"
+  },
+  {
+    value: 2,
+    suit: "♢"
+  },
+  {
+    value: 10,
+    suit: "♢"
+  },
+  {
+    value: 1,
+    suit: "♡"
+  },
+  {
+    value: 3,
+    suit: "♡"
+  }
 ];
 
-function compare(a, b) {
-  if (a < b) {
-    return -1;
-  } else if (a > b) {
-    return 1;
-  } else {
-    return 0;
-  }
-}
+//function compare(a, b) {
+//  if (a < b) {
+//    return -1;
+//  } else if (a > b) {
+//    return 1;
+//  } else {
+//    return 0;
+//  }
+//}
 
-deck.sort(compare);
+//deck.sort(compare);
+
 /*
 Sort the deck of cards so that it becomes [1,1,1,1,2,2...]
 */
--- tests.js
+++ tests.js
@@ -58,36 +58,42 @@ function dealCards() {
   document.querySelector("button").addEventListener("click", test)
 }
 
-
-
 function test() {
-  let counter = 0;
-  const intID = setInterval(() => {
-    counter++;
-    document.querySelectorAll(`[data-cardvalue="${counter}"]`).forEach(card => {
-      card.classList.add("flipped");
-    })
-    if (counter === 13) {
-      clearInterval(intID);
-      setTimeout(validate, 500);
+  const template = document.querySelector("template").content;
+  deck.forEach(card => {
+    const copy = template.cloneNode(true);
+    copy.querySelector("h1").innerHTML = card.value + card.suit;
+    copy.querySelector(".flip-card").dataset.cardvalue = card.value
+    document.querySelector(".ex1").appendChild(copy);
+  })
+  const test = function () {
+    let counter = 0;
+    const intID = setInterval(() => {
+      counter++;
+      document.querySelectorAll(`[data-cardvalue="${counter}"]`).forEach(card => {
+        card.classList.add("flipped");
+      })
+      if (counter === 13) {
+        clearInterval(intID);
+        setTimeout(validate, 500);
 
-    }
-  }, 300)
+      }
+    }, 300)
 
-  function validate() {
-    const copy = [...deck];
-    copy.sort((a, b) => a - b);
-    let equal = true;
-    for (let i = 0; i < copy.length; i++) {
-      if (deck[i] != copy[i]) {
-        equal = false;
-        break;
+    function validate() {
+      const copy = [...deck];
+      copy.sort((a, b) => a.value - b.value);
+      let equal = true;
+      for (let i = 0; i < copy.length; i++) {
+        if (deck[i].value != copy[i].value) {
+          equal = false;
+          break;
+        }
+      }
+      if (!equal) {
+        document.body.innerHTML = "EPIC FAIL!!!"
+      } else {
+        document.body.innerHTML = "EPIC WIN!!!"
       }
     }
-    if (!equal) {
-      document.body.innerHTML = "EPIC FAIL!!!"
-    } else {
-      document.body.innerHTML = "EPIC WIN!!!"
-    }
   }
-}
